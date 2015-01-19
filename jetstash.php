5d775288bdc2b20d8c3c6e4280c71bfe45ef36ba
Patrick Shampine <pshampine@gunnjerkens.com>
2015-01-19 11:33:38 -0800

    Working outline, admin panel page

--- jetstash.php
+++ jetstash.php
@@ -8,14 +8,17 @@
  * Author URI: https://www.jetstash.com
  */
 
-class jetstashConnect
+class JetstashConnect
 {
 
   /**
    * Define the private class vars
    *
    * @var $version string
+   * @var $wp_version string
+   * @var settings object||false
    */
+  private $version, $settings;
 
   /**
    * Construct function
@@ -25,20 +28,158 @@ class jetstashConnect
   function __construct()
   {
     $this->version = '0.1.0';
+    add_action('admin_init', array($this, 'checkVersion'));
+    if(!$this->compatibleVersion()) return;
+
     $this->setVersion();
+    $this->setSettings();
+    add_action('admin_menu', array(&$this,'loadAdminPanel'));
   }
 
+  /**
+   * Sets our version number in the wp_options table
+   *
+   * @return void
+   */
   protected function setVersion()
   {
     $version = get_option('jetstash_connect_version');
     if($version !== $this->version) {
       update_option('jetstash_connect_version', $this->version);
     }
+  }
+
+  /**
+   * Get our settings from the wp_options table
+   *
+   * @return void
+   */
+  protected function setSettings()
+  {
+    $settings = get_option('jetstash_connect_settings');
+    if($settings !== false) {
+      $this->settings = unserialize($settings);
+    } else {
+      $this->settings = false;
+    }
+  }
+
+  /**
+   * Check to make sure current state of environment meets plugin needs
+   *
+   * @return bool
+   */
+  static function compatibleVersion()
+  {
+    if(version_compare($GLOBALS['wp_version'], '3.9', '<')) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Check the WordPress version against what the plugin supports
+   *
+   * @return void
+   */
+  function checkVersion()
+  {
+    if(!self::compatibleVersion()) {
+      if(is_plugin_active(plugin_basename(__FILE__))) {
+        deactivate_plugins(plugin_basename( __FILE__ ));
+        add_action('admin_notices', array($this, 'pluginDisabled'));
+        if(isset($_GET['activate'])) unset($_GET['activate']);
+      }
+    }
+  }
+
+  /**
+   * Disabled plugin messaging
+   *
+   * @return string
+   */
+  function pluginDisabled()
+  {
+    echo '<strong>'.esc_html__('Jetstash Connect requires one of the latest 3 versions of WordPress.', 'JetstashConnect').'</strong>';
+  }
+
+  /**
+   * Activation check
+   *
+   *
+   *
+   */
+  static function activationCheck()
+  {
+    if(!self::compatibleVersion()) {
+      deactivate_plugins(plugin_basename(__FILE__));
+      wp_die(__('Jetstash Connect requires one of the latest 3 versions of WordPress.', 'JetstashConnect'));
+    }
+  }
+
+  /**
+   * Load the options panel
+   *
+   * @return void
+   */
+  function loadAdminPanel() {
+    add_options_page( 'Jetstash Connect', 'Jetstash Connect', 'administrator', 'jetstash_connect', array(&$this,'loadAdminPanelTemplates'));
+  }
+
+  /**
+   * Load the admin options panel templates
+   *
+   * @return void
+   */
+  function loadAdminPanelTemplates() {
+    include('admin/options.php');
+  }
+
+  /**
+   * Retrieves a users available forms from via the Jetstash API
+   *
+   * @return void
+   */
+  protected function retrieveForms()
+  {
 
   }
 
+  /**
+   * Retrieves the field sets for a single form field
+   *
+   * @param string
+   *
+   * @return 
+   */
+  protected function retrieveSingleFormFields($formId)
+  {
 
 
+
+  }
+
+  /**
+   * Perform the api get requests
+   *
+   * @param string
+   *
+   * @return object
+   */
+  private function handleGetRequest($endpoint = null)
+  {
+    $url = 'http://api.jetstash.dev/v1/user/forms?api_key=123&user=1';
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+    $data = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($data);
+  }
 
 }
-new jetstashConnect();
+new JetstashConnect();
+
+register_activation_hook(__FILE__, array('JetstashConnect', 'activationCheck'));
