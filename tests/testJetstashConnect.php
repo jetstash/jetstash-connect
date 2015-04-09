a5bcfbe8f53e1fe40b1347ad32a1971271a5c439
Patrick Shampine <pshampine@gunnjerkens.com>
2015-04-09 16:40:28 -0700

    Refactor to push settings into application on every test

--- tests/testJetstashConnect.php
+++ tests/testJetstashConnect.php
@@ -7,7 +7,7 @@ class JetstashConnectTest extends WP_UnitTestCase {
    *
    * @var class
    */
-  private $jetstash, $config, $settings, $apiUrl;
+  private $jetstash, $settings, $api_url;
 
   /**
    * Constructor function
@@ -15,10 +15,11 @@ class JetstashConnectTest extends WP_UnitTestCase {
    */
   function __construct()
   {
+    $this->setSettings();
     $this->jetstash = new JetstashConnect();
     $this->jetstash->test = true;
-    $this->getConfig();
-    $this->setSettings();
+    $this->jetstash->settings = $this->settings;
+
     $this->setApiUrl();
   }
 
@@ -26,38 +27,39 @@ class JetstashConnectTest extends WP_UnitTestCase {
    * Set our settings variable
    *
    */
-  private function getConfig() {
-    $envs = array('local', 'staging');
-    foreach($envs as $env) {
-      if(file_exists(dirname( __FILE__ ).'/../env_'.$env)) {
-        $this->config = file_get_contents(realpath(__DIR__.'/../env_'.$env));
-        $this->config = json_decode($this->config);
-        break;
-      } 
-    }
-    if(isset($_SERVER['environment']) && 'travis' === $_SERVER['environment']) {
-      $this->config->api_url         = isset($_SERVER['api_url']) ? $_SERVER['api_url'] : null;
-      $this->config->api_key         = isset($_SERVER['api_key']) ? $_SERVER['api_key'] : null;
-      $this->config->form_id         = isset($_SERVER['form_id']) ? $_SERVER['form_id'] : null;
-      $this->config->user            = isset($_SERVER['user']) ? $_SERVER['user'] : null;
-      $this->config->success_message = isset($_SERVER['success_message']) ? $_SERVER['success_message'] : null;
-      $this->config->cache_duration  = isset($_SERVER['cache_duration']) ? $_SERVER['cache_duration'] : null;
-    }
-  }
-
   private function setSettings() {
-    $this->settings['api_key']            = $this->config->api_key;
-    $this->settings['user']               = $this->config->user;
-    $this->settings['success_message']    = $this->config->success_message;
-    $this->settings['cache_duration']     = $this->config->cache_duration;
-    $this->settings['disable_stylesheet'] = true;
-    $this->settings['invalidate_cache']   = false;
-
-    $this->jetstash->settings = (object) $this->settings;
+    $this->settings = new StdClass();
+    if(isset($_SERVER['environment']) && 'travis' === $_SERVER['environment']) {
+      $this->settings->api_url         = isset($_SERVER['api_url']) ? $_SERVER['api_url'] : null;
+      $this->settings->api_key         = isset($_SERVER['api_key']) ? $_SERVER['api_key'] : null;
+      $this->settings->form_id         = isset($_SERVER['form_id']) ? $_SERVER['form_id'] : null;
+      $this->settings->user            = isset($_SERVER['user']) ? $_SERVER['user'] : null;
+      $this->settings->success_message = isset($_SERVER['success_message']) ? $_SERVER['success_message'] : null;
+      $this->settings->cache_duration  = isset($_SERVER['cache_duration']) ? $_SERVER['cache_duration'] : null;
+    } else {
+      $envs = array('local', 'staging');
+      foreach($envs as $env) {
+        if(file_exists(dirname( __FILE__ ).'/../env_'.$env)) {
+          $config = file_get_contents(realpath(__DIR__.'/../env_'.$env));
+          $config = json_decode($config);
+          break;
+        }
+      }
+      if($config) {
+        $this->settings->api_url         = isset($config->api_url) ? $config->api_url : null;
+        $this->settings->api_key         = isset($config->api_key) ? $config->api_key : null;
+        $this->settings->form_id         = isset($config->form_id) ? $config->form_id : null;
+        $this->settings->user            = isset($config->user) ? $config->user : null;
+        $this->settings->success_message = isset($config->success_message) ? $config->success_message : null;
+        $this->settings->cache_duration  = isset($config->cache_duration) ? $config->cache_duration : null;
+      } else {
+        die('Tests cannot run without config environments');
+      }
+    }
   }
 
   private function setApiUrl() {
-    $this->apiUrl = $this->config->api_url.'/v1/user/forms?api_key='.$this->config->api_key.'&user='.$this->config->user;
+    $this->api_url = $this->settings->api_url.'/v1/user/forms?api_key='.$this->settings->api_key.'&user='.$this->settings->user;
   }
 
   /**
@@ -66,7 +68,7 @@ class JetstashConnectTest extends WP_UnitTestCase {
    */
   function testUpdateSettings() 
   {
-    $settings = JetstashConnect::updateSettings($this->settings);
+    $settings = JetstashConnect::updateSettings((array) $this->settings);
 
     // Assert our object has the expected attributes
     $attributes = array('api_key', 'user', 'success_message', 'cache_duration', 'disable_stylesheet', 'invalidate_cache', 'error', 'error_message');
@@ -92,8 +94,7 @@ class JetstashConnectTest extends WP_UnitTestCase {
    */
   function testBuildStructure()
   {
-    $form = array('form' => $this->config->form_id);
-    $this->jetstash->settings = (object) $this->settings;
+    $form = array('form' => $this->settings->form_id);
     $response = $this->jetstash->buildStructure($form);
 
     $this->assertInternalType('string', $response);
@@ -108,7 +109,7 @@ class JetstashConnectTest extends WP_UnitTestCase {
     $data = array(
       'nonce' => 'fake_nonce',
       'post'  => 'first_middle_last_name=&first_name=first&last_name=last&email=test@example.com',
-      'form'  => $this->config->form_id,
+      'form'  => $this->settings->form_id,
     );
     $response   = json_decode($this->jetstash->submitForm($data));
     $attributes = array('success', 'message', 'data');
